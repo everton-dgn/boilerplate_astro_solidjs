0e2c8d21bb125976e372639c3921dcb0746150ff
Éverton Toffanetto <evertondgn@hotmail.com>
2025-03-01 04:13:23 -0300

    fix: update viewport meta tag for better responsiveness and adjust test case for consistency

--- src/hooks/useCount/__tests__/useCount.test.ts
+++ src/hooks/useCount/__tests__/useCount.test.ts
@@ -3,7 +3,7 @@ import { useCount } from '..'
 import { renderHooksProvider } from 'tests/providers/hook'
 
 describe('[Hook] useCount', () => {
-  it('should increment when call count', async () => {
+  it('should increment when call count', () => {
     const { result } = renderHooksProvider(() => useCount())
 
     expect(result.count()).toBe(0)
