a854461ceec0494ac16d56a54afad140c7f43123
Éverton Toffanetto <evertondgn@hotmail.com>
2025-05-18 00:52:18 -0300

    test(home): remove CounterButton interaction test
    Removes redundant CounterButton interaction test from home.spec.ts. Streamlines test suite and eliminates unnecessary checks.

--- src/tests/pages/home.spec.ts
+++ src/tests/pages/home.spec.ts
@@ -11,16 +11,4 @@ test.describe('navigation', () => {
     const header = page.locator('header')
     await expect(header).toHaveText('Boilerplate')
   })
-
-  test('must interact with the CounterButton', async ({ page }) => {
-    await page.click('button[aria-label="COUNT: 0"]')
-
-    const buttonText = await page.innerText('button[aria-label="COUNT: 1"]')
-    expect(buttonText).toBe('COUNT: 1')
-
-    await page.getByRole('button', { name: 'COUNT: 1' }).click()
-
-    const buttonText2 = page.getByRole('button', { name: 'COUNT: 2' })
-    await expect(buttonText2).toBeVisible()
-  })
 })
